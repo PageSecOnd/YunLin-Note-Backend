eec4133c834406db2e1413bb5120f2c04f0eda77
PageSecOnd <jamson@fighton.onmicrosoft.com>
2025-06-20 17:59:19 +0800

    Initial commit

--- api/notes.js
+++ api/notes.js
@@ -2,6 +2,16 @@
 let noteContent = '';
 
 export default async function handler(req, res) {
+    // 设置 CORS 头
+    res.setHeader('Access-Control-Allow-Origin', 'https://note.yunlinsan.ren');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
+    // 处理预检请求
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method === 'GET') {
         // 获取笔记内容
         res.status(200).json({ content: noteContent });
@@ -15,7 +25,7 @@ export default async function handler(req, res) {
             res.status(400).json({ error: 'Invalid content' });
         }
     } else {
-        res.setHeader('Allow', ['GET', 'POST']);
+        res.setHeader('Allow', ['GET', 'POST', 'OPTIONS']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
